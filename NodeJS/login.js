86b87e67528e8524276c1db6889560fb96959eb8
Ziinebfathi <zfathi@miu.edu>
2021-06-28 20:47:45 -0500

    W2D1

---
+++ NodeJS/login.js
@@ -0,0 +1,13 @@
+function handleSubmit() {
+    console.log("Lab 04 ===================== ");
+    let form = document.getElementById("loginForm");
+    console.log("Email: " + form.elements['email'].value);
+    console.log("Password: " + form.elements['password'].value);
+    console.log("Website URL: " + form.elements['webUrl'].value);
+    console.log("Check me out: " + document.getElementById("loginForm").elements['checkMeOut'].checked);
+
+    console.log("\n\n\nLab 09 ===================== ");
+    console.log("Email: " + $('#email').val());
+    console.log("Password: " + $('#password').val());
+    return false;
+}
