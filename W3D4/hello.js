db6f16da7019418c7a26eab998182579add34b26
Ziinebfathi <zfathi@miu.edu>
2021-07-09 08:08:54 -0500

    nodejs

---
+++ W3D4/hello.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name ? ", function(name) {
+    rl.question("how old are you ? ", function(age) {
+        if(age < 16){
+            console.log('“You’re notallowed to drive in Iowa')
+          }else{
+            console.log('you are allowed to get a drivers license in iowa ')
+          }
+        rl.close();
+    });
+});
+
+rl.on("close", function() {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
